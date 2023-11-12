f6df6819e2d63d70b557a40583d9fe57d3fc1dcb
LidaKalina <146226880+LidaKalina@users.noreply.github.com>
2023-11-12 23:05:04 +0500

    Add files via upload

---
+++ buns/mod7/task3.py
@@ -0,0 +1,45 @@
+import time
+
+
+def timer(func):
+
+    def wrapped_func(*args):
+        started_at = time.time()
+        result = func(*args)
+        ended_at = time.time()
+        run_time = ended_at - started_at
+        print('Функция работала {} секунд(ы)'.format(run_time))
+        return result
+
+    return wrapped_func
+
+'''
+def memoize(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    wrapper.__name__ = func.__name__
+    wrapper.__doc__ = func.__doc__
+    return wrapper
+
+
+@timer
+@memoize
+def fibonacci_memoise(n):
+    if n < 2:
+        return n
+    return fibonacci_memoise(n - 1) + fibonacci_memoise(n - 2)
+'''
+
+@timer
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(100))
+#print(fibonacci_memoise(100))
