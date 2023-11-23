336f5c5d3e8fc16d9e1b1a85d73e44a911ea25db
LidaKalina <146226880+LidaKalina@users.noreply.github.com>
2023-11-23 23:33:55 +0500

    Add files via upload

---
+++ pract/mod10/task1.py
@@ -0,0 +1,34 @@
+import re
+import doctest
+
+def is_correct_password(password):
+    """
+    Function for checking password
+
+    >>> is_correct_password('rtG3FG!Tr^e')
+    True
+    >>> is_correct_password('aA1!*!1Aa')
+    True
+    >>> is_correct_password('oF^a1D@y5e6')
+    True
+    >>> is_correct_password('enroi#$rkdeR#$092uWedchf34tguv394h')
+    True
+    >>> is_correct_password('пароль')
+    False
+    >>> is_correct_password('password')
+    False
+    >>> is_correct_password('qwerty')
+    False
+    >>> is_correct_password('lOngPa$$$W0Rd')
+    False
+
+    """
+    pattern = r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z]{2,})([^$%@#&*!?]{2,}).{6,}'
+    if re.fullmatch(pattern, password):
+        return True
+    else:
+        return False
+
+
+doctest.testmod()
+print(is_correct_password('rtG3FG!Tr^e'))
